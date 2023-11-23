43be7dfc18278494db0b71898bc78de2bf7d69b1
Matvii Voznosimenko <matvii.voznosimenko@onix-systems.com>
2023-11-23 13:04:21 +0200

    refactor(types): removed unnecessary fields from token types

--- src/modules/auth/entities/tokens.entity.ts
+++ src/modules/auth/entities/tokens.entity.ts
@@ -1,19 +1,7 @@
 import { TokenWhiteList } from '../types/tokens-white-list.type';
 
 export class TokensEntity implements TokenWhiteList {
-  readonly id: string;
-
-  readonly userId: string;
-
   readonly accessToken: string | null;
 
   readonly refreshToken: string | null;
-
-  readonly refreshTokenId: string | null;
-
-  readonly expiredAt: Date;
-
-  readonly createdAt: Date;
-
-  readonly updatedAt: Date;
 }
--- src/modules/auth/types/tokens-white-list.type.ts
+++ src/modules/auth/types/tokens-white-list.type.ts
@@ -1,10 +1,4 @@
 export type TokenWhiteList = {
-  id: string;
-  userId: string;
   accessToken: string | null;
   refreshToken: string | null;
-  refreshTokenId: string | null;
-  expiredAt: Date;
-  createdAt: Date;
-  updatedAt: Date;
 };
