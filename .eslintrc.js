d606f4864cfbb0f03766054a29349d5690fca6c3
Matvii Voznosimenko <matvii.voznosimenko@onix-systems.com>
2023-11-23 15:53:26 +0200

    refactor(eslintrc): padding rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,9 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/ban-types': 'off',
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: '*', next: 'return' },
+    ],
   },
 };
