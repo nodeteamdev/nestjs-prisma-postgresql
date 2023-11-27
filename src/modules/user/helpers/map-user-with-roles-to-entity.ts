88c48b67fa47872901a3c7421609c2490e55881d
Matvii Voznosimenko <matvii.voznosimenko@onix-systems.com>
2023-11-27 10:19:43 +0200

    refactor(comment): remove unnecessary comment

--- src/modules/user/helpers/map-user-with-roles-to-entity.ts
+++ src/modules/user/helpers/map-user-with-roles-to-entity.ts
@@ -4,8 +4,7 @@ import UserEntity from '../entities/user.entity';
 export const mapUserWithRolesToEntity = (
   userWithRoles: UserWithRoles,
 ): UserEntity => {
-  // Assuming 'roles' is an array in the actual data
-  const roles = userWithRoles.roles?.map((userRole) => userRole.role) || null;
+  const roles = userWithRoles.roles.map((userRole) => userRole.role);
 
   return {
     ...userWithRoles,
